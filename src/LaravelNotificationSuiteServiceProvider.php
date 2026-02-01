f39d00d5a17e9a6c3694114872a0a92cdd4d86f6
bamilawal <delasoftng@gmail.com>
2026-02-01 22:30:42 +0100

    first commit

---
+++ src/LaravelNotificationSuiteServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Blearnd\LaravelNotificationSuite;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogPackageServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+      $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravelnotificationsuite');
+
+  }
+
+  public function boot()
+  {
+    if ($this->app->runningInConsole()) {
+
+        $this->publishes([
+        __DIR__.'/../config/config.php' => config_path('laravel-notification-suite.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-notification-suite'),
+        ], 'views');
+
+        // Publish view components
+        $this->publishes([
+            __DIR__.'/../src/View/Components/' => app_path('View/Components'),
+            __DIR__.'/../resources/views/components/' => resource_path('views/components'),
+        ], 'view-components');
+
+    }
+
+    $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravelnotificationsuite');
+
+  }
+}
